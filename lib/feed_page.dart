94f6b380bd4f7d441b0cb350ff454feefbb796f7
✔   seungmin <45160518+seungminlll3420@users.noreply.github.com>
2020-09-08 22:12:35 +0900

    create feed_page

---
+++ lib/feed_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Feedpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Instagram clone',
+          style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
