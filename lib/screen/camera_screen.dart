cd6dd3809d1d8408592275ca8ecdb1e133add069
✔   seungmin <45160518+seungminlll3420@users.noreply.github.com>
2020-09-26 20:43:26 +0900

    Update camera_screen.dart
    add animation

--- lib/screen/camera_screen.dart
+++ lib/screen/camera_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clon_2/screen/profile_screen.dart';
 
 class CameraScrren extends StatefulWidget {
   @override
@@ -6,10 +7,75 @@ class CameraScrren extends StatefulWidget {
 }
 
 class _CameraScrrenState extends State<CameraScrren> {
+  int _currentIndex = 1;
+  PageController _pageController = PageController(initialPage: 1);
+  String _title = 'PHOTO';
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amberAccent,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: [
+          Container(
+            color: Colors.amber,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+        ],
+        onPageChanged: (index) {
+          setState(() {
+            switch (index) {
+              case 0:
+                _title = 'GALLERY';
+                break;
+              case 1:
+                _title = 'PHOTO';
+                break;
+              case 2:
+                _title = 'VIDEO';
+                break;
+            }
+            _currentIndex = index;
+          });
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          iconSize: 0,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          selectedItemColor: Colors.black87,
+          unselectedItemColor: Colors.black54,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.ac_unit), title: Text('GALLERY')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.ac_unit), title: Text('PHOTO')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.ac_unit), title: Text('VIDEO')),
+          ],
+          currentIndex: _currentIndex,
+          onTap: _onItemTabbed),
     );
+  }
+
+  void _onItemTabbed(index) {
+    setState(() {
+      _currentIndex = index;
+      _pageController.animateToPage(_currentIndex,
+          duration: duration, curve: Curves.fastOutSlowIn);
+    });
   }
 }
