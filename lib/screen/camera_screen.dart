eda2a19300347888bcc845d37fd80ac2cefd9133
✔   seungmin <45160518+seungminlll3420@users.noreply.github.com>
2020-09-26 19:57:20 +0900

    Create camera_screen.dart

---
+++ lib/screen/camera_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CameraScrren extends StatefulWidget {
+  @override
+  _CameraScrrenState createState() => _CameraScrrenState();
+}
+
+class _CameraScrrenState extends State<CameraScrren> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amberAccent,
+    );
+  }
+}
