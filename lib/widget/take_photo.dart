ec72f16be0be34f42c952e54429fb8fac2a32bd5
✔   seungmin <45160518+seungminlll3420@users.noreply.github.com>
2020-09-29 23:47:28 +0900

    Update take_photo.dart
    use CameraState

--- lib/widget/take_photo.dart
+++ lib/widget/take_photo.dart
@@ -1,7 +1,9 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clon_2/constants/screen_size.dart';
+import 'package:instagram_clon_2/model/camera_state.dart';
 import 'package:instagram_clon_2/widget/my_progress_indicator.dart';
+import 'package:provider/provider.dart';
 
 class TakePhoto extends StatefulWidget {
   const TakePhoto({
@@ -14,21 +16,20 @@ class TakePhoto extends StatefulWidget {
 
 class _TakePhotoState extends State<TakePhoto> {
   Widget _progress = MyProgressIndicator();
-  CameraController _controller;
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<CameraDescription>>(
-      future: availableCameras(),
-      builder: (context, snapshot) {
+    return Consumer<CameraState>(
+      builder: (context, CameraState cameraState, child) {
         return Column(
           children: <Widget>[
             Container(
               width: size.width,
               height: size.width,
               color: Colors.green,
-              child:
-                  (snapshot.hasData) ? _getPreview(snapshot.data) : _progress,
+              child: (cameraState.isReadyToTakePhoto)
+                  ? _getPreview(cameraState)
+                  : _progress,
             ),
             Expanded(
                 child: OutlineButton(
@@ -42,28 +43,18 @@ class _TakePhotoState extends State<TakePhoto> {
     );
   }
 
-  Widget _getPreview(List<CameraDescription> camera) {
-    _controller = CameraController(camera[0], ResolutionPreset.medium);
-    return FutureBuilder(
-      future: _controller.initialize(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return ClipRect(
-            child: OverflowBox(
-              alignment: Alignment.center,
-              child: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Container(
-                    width: size.width,
-                    height: size.width / _controller.value.aspectRatio,
-                    child: CameraPreview(_controller)),
-              ),
-            ),
-          );
-        } else {
-          return _progress;
-        }
-      },
+  Widget _getPreview(CameraState cameraState) {
+    return ClipRect(
+      child: OverflowBox(
+        alignment: Alignment.center,
+        child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Container(
+              width: size.width,
+              height: size.width / cameraState.controller.value.aspectRatio,
+              child: CameraPreview(cameraState.controller)),
+        ),
+      ),
     );
   }
 }
